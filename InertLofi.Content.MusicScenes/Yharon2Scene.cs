9c05bb03492108304d8a724ef1fcb09369d48fba
Aero-GamingPc <aerop@LAPTOP-0T9KKG8M>
2025-04-20 18:47:36 +0200

    Added Yharon2 (hopefully working)

---
+++ InertLofi.Content.MusicScenes/Yharon2Scene.cs
@@ -0,0 +1,31 @@
+﻿using CalamityMod.Events;
+using CalamityMod.NPCs.DevourerofGods;
+using CalamityMod.NPCs.Yharon;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace InertLofi.Content.MusicScenes;
+
+public class Yharon2Scene : ModSceneEffect
+{
+    public override int Music => MusicLoader.GetMusicSlot((Mod)(object)InertLofiMod.Instance, "Assets/Music/Yharon2");
+
+    public override SceneEffectPriority Priority => (SceneEffectPriority)8;
+
+    public override bool IsSceneEffectActive(Player player)
+    {
+        int wantedId = ModContent.NPCType<Yharon>();
+        bool inPhase2 = false;
+        for (int i = 0; i < 200; i++)
+        {
+            if (Main.npc[i].active && Main.npc[i].type == wantedId)
+            {
+                if ((double)Main.npc[i].GetLifePercent() <= 0.55)
+                {
+                    inPhase2 = true;
+                }
+            }
+        }
+        return inPhase2 && !BossRushEvent.BossRushActive;
+    }
+}
