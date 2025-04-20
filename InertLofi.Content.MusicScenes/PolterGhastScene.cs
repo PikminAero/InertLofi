b85965be5de23fa2edcda64e1425dc4a8ccb5d52
Aero-GamingPc <aerop@LAPTOP-0T9KKG8M>
2025-04-20 17:42:47 +0200

    Added Polterghast

---
+++ InertLofi.Content.MusicScenes/PolterGhastScene.cs
@@ -0,0 +1,18 @@
+﻿using CalamityMod.Events;
+using CalamityMod.NPCs.Polterghast;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace InertLofi.Content.MusicScenes;
+
+public class PolterghastScene : ModSceneEffect
+{
+    public override int Music => MusicLoader.GetMusicSlot((Mod)(object)InertLofiMod.Instance, "Assets/Music/Polterghast");
+
+    public override SceneEffectPriority Priority => (SceneEffectPriority)8;
+
+    public override bool IsSceneEffectActive(Player player)
+    {
+        return NPC.AnyNPCs(ModContent.NPCType<Polterghast>()) && !BossRushEvent.BossRushActive;
+    }
+}
