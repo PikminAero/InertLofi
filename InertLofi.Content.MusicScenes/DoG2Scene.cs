29e53d679571acbb1acba80305c399f25336e621
Aero-GamingPc <aerop@LAPTOP-0T9KKG8M>
2025-04-20 18:27:03 +0200

    Added Yharon1, and fixed incorrect song name for DoG2

--- InertLofi.Content.MusicScenes/DoG2Scene.cs
+++ InertLofi.Content.MusicScenes/DoG2Scene.cs
@@ -7,7 +7,7 @@ namespace InertLofi.Content.MusicScenes;
 
 public class DoG2Scene : ModSceneEffect
 {
-    public override int Music => MusicLoader.GetMusicSlot((Mod)(object)InertLofiMod.Instance, "Assets/Music/DoG1");
+    public override int Music => MusicLoader.GetMusicSlot((Mod)(object)InertLofiMod.Instance, "Assets/Music/DoG2");
 
     public override SceneEffectPriority Priority => (SceneEffectPriority)8;
 
---
+++ InertLofi.Content.MusicScenes/Yharon1Scene.cs
@@ -0,0 +1,19 @@
+﻿using CalamityMod.Events;
+using CalamityMod.NPCs.DevourerofGods;
+using CalamityMod.NPCs.Yharon;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace InertLofi.Content.MusicScenes;
+
+public class Yharon1Scene : ModSceneEffect
+{
+    public override int Music => MusicLoader.GetMusicSlot((Mod)(object)InertLofiMod.Instance, "Assets/Music/Yharon1");
+
+    public override SceneEffectPriority Priority => (SceneEffectPriority)8;
+
+    public override bool IsSceneEffectActive(Player player)
+    {
+        return NPC.AnyNPCs(ModContent.NPCType<Yharon>()) && !BossRushEvent.BossRushActive;
+    }
+}
