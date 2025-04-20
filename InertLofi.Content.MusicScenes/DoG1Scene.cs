6c4af94e1fec217b9889b2308565d632882d5294
Aero-GamingPc <aerop@LAPTOP-0T9KKG8M>
2025-04-20 18:20:11 +0200

    Added DoG phase 1, and incomplete DoG phase 2

---
+++ InertLofi.Content.MusicScenes/DoG1Scene.cs
@@ -0,0 +1,18 @@
+﻿using CalamityMod.Events;
+using CalamityMod.NPCs.DevourerofGods;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace InertLofi.Content.MusicScenes;
+
+public class DoG1Scene : ModSceneEffect
+{
+    public override int Music => MusicLoader.GetMusicSlot((Mod)(object)InertLofiMod.Instance, "Assets/Music/DoG1");
+
+    public override SceneEffectPriority Priority => (SceneEffectPriority)8;
+
+    public override bool IsSceneEffectActive(Player player)
+    {
+        return NPC.AnyNPCs(ModContent.NPCType<DevourerofGodsHead>()) && !BossRushEvent.BossRushActive;
+    }
+}
---
+++ InertLofi.Content.MusicScenes/DoG2Scene.cs
@@ -0,0 +1,30 @@
+﻿using CalamityMod.Events;
+using CalamityMod.NPCs.DevourerofGods;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace InertLofi.Content.MusicScenes;
+
+public class DoG2Scene : ModSceneEffect
+{
+    public override int Music => MusicLoader.GetMusicSlot((Mod)(object)InertLofiMod.Instance, "Assets/Music/DoG1");
+
+    public override SceneEffectPriority Priority => (SceneEffectPriority)8;
+
+    public override bool IsSceneEffectActive(Player player)
+    {
+        int wantedId = ModContent.NPCType<DevourerofGodsHead>();
+        bool inPhase2 = false;
+        for (int i = 0; i < 200; i++)
+        {
+            if (Main.npc[i].active && Main.npc[i].type == wantedId)
+            {
+                if ((DevourerofGodsHead)Main.npc[i].Phase2Started)
+                {
+                    inPhase2 = true;
+                }
+            }
+        }
+        return inPhase2 && !BossRushEvent.BossRushActive;
+    }
+}
