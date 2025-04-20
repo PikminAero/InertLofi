4633a5ca9a3fa052ad92e86d56c56c464d7edaaa
Aero-GamingPc <aerop@LAPTOP-0T9KKG8M>
2025-04-20 18:50:23 +0200

    Added Threats of the Ocean Floor

---
+++ InertLofi.Content.MusicScenes/WyrmScene.cs
@@ -0,0 +1,25 @@
+﻿using CalamityMod.Events;
+using CalamityMod.NPCs.Abyss;
+using CalamityMod.NPCs.DevourerofGods;
+using CalamityMod.NPCs.PrimordialWyrm;
+using CalamityMod.NPCs.Yharon;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace InertLofi.Content.MusicScenes;
+
+public class WyrmScene : ModSceneEffect
+{
+    public override int Music => MusicLoader.GetMusicSlot((Mod)(object)InertLofiMod.Instance, "Assets/Music/Yharon1");
+
+    public override SceneEffectPriority Priority => (SceneEffectPriority)8;
+
+    public override bool IsSceneEffectActive(Player player)
+    {
+        return NPC.AnyNPCs(ModContent.NPCType<EidolonWyrmHead>()) 
+            || NPC.AnyNPCs(ModContent.NPCType<ColossalSquid>())
+            || NPC.AnyNPCs(ModContent.NPCType<ReaperShark>())
+            || NPC.AnyNPCs(ModContent.NPCType<PrimordialWyrmHead>())
+            && !BossRushEvent.BossRushActive;
+    }
+}
