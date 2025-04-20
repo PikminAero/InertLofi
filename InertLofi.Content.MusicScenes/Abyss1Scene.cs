49c59e7037d95554ebe4dcc2bef54dde108e6309
Aero-GamingPc <aerop@LAPTOP-0T9KKG8M>
2025-04-20 19:16:18 +0200

    Added all Abyss songs, Sunken Sea and Brimstone Crags

---
+++ InertLofi.Content.MusicScenes/Abyss1Scene.cs
@@ -0,0 +1,18 @@
+﻿using CalamityMod.BiomeManagers;
+using CalamityMod.BiomeManagers.BestiaryCategories;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace InertLofi.Content.MusicScenes;
+
+public class Abyss1Scene : ModSceneEffect
+{
+    public override int Music => MusicLoader.GetMusicSlot((Mod)(object)InertLofiMod.Instance, "Assets/Music/Abyss1");
+
+    public override SceneEffectPriority Priority => (SceneEffectPriority)8;
+
+    public override bool IsSceneEffectActive(Player player)
+    {
+        return player.InModBiome<AbyssLayer1Biome>();
+    }
+}
---
+++ InertLofi.Content.MusicScenes/BrimCragScene.cs
@@ -0,0 +1,19 @@
+﻿using CalamityMod.BiomeManagers;
+using CalamityMod.BiomeManagers.BestiaryCategories;
+using CalamityMod.Scenes.MusicScenes;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace InertLofi.Content.MusicScenes;
+
+public class BrimCragScene : ModSceneEffect
+{
+    public override int Music => MusicLoader.GetMusicSlot((Mod)(object)InertLofiMod.Instance, "Assets/Music/BrimCrag");
+
+    public override SceneEffectPriority Priority => (SceneEffectPriority)8;
+
+    public override bool IsSceneEffectActive(Player player)
+    {
+        return player.InModBiome<BioLabMusicScene>();
+    }
+}
