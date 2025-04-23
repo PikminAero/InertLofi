859cabe1745f37076b53cd670ca0cad1e1bb6436
Aero-GamingPc <aerop@LAPTOP-0T9KKG8M>
2025-04-23 22:36:04 +0200

    Added Devourer of Gods phase 1 and 2

---
+++ InertLofi.Content.MusicScene/DoGPhase1Scene.cs
@@ -0,0 +1,22 @@
+﻿using CalamityMod.Events;
+using CalamityMod.NPCs.DevourerofGods;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace InertLofi.Content.MusicScenes;
+
+public class DoG1PhaseScene : ModSceneEffect
+{
+    public override int Music => MusicLoader.GetMusicSlot((Mod)(object)InertLofiMod.Instance, "Assets/Music/DevourerofGodsPhase1");
+
+    public override SceneEffectPriority Priority => SceneEffectPriority.BossHigh;
+
+    public override float GetWeight(Player player)
+    {
+        return 1f;
+    }
+    public override bool IsSceneEffectActive(Player player)
+    {
+        return NPC.AnyNPCs(ModContent.NPCType<DevourerofGodsHead>()) && !BossRushEvent.BossRushActive;
+    }
+}
