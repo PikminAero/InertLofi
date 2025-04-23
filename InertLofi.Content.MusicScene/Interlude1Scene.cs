8907fb304f94947af9dc625bbbea1c6b2fed2c68
Aero-GamingPc <aerop@LAPTOP-0T9KKG8M>
2025-04-23 22:47:36 +0200

    Wrote 1st version of Interlude scene

---
+++ InertLofi.Content.MusicScene/Interlude1Scene.cs
@@ -0,0 +1,28 @@
+﻿using CalamityMod.Events;
+using CalamityMod.NPCs.HiveMind;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace InertLofi.Content.MusicScenes;
+
+public class Interlude1Scene : ModSceneEffect
+{
+    public override int Music => MusicLoader.GetMusicSlot((Mod)(object)InertLofiMod.Instance, "Assets/Music/Interlude1");
+
+    public override SceneEffectPriority Priority => SceneEffectPriority.BiomeHigh;
+    public override float GetWeight(Player player)
+    {
+        return 1f;
+    }
+    public override bool IsSceneEffectActive(Player player)
+    {
+        Mod cache = new Mod();
+        // If we can't find CalamityMod, then the scene cannot take place and this method returns false
+        if (!ModLoader.TryGetMod("CalamityMod", out cache))
+        {
+            return false;
+        }
+        // Return true if the music that's currently playing is the Interlude song
+        return (Main.curMusic == MusicLoader.GetMusicSlot(cache, "Sounds/Music/Interlude1") && !BossRushEvent.BossRushActive;
+    }
+}
