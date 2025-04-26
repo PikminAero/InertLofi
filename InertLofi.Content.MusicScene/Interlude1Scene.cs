0d061f485afc6b298cc4162b440aa4c5b5703307
Aero-GamingPc <aerop@LAPTOP-0T9KKG8M>
2025-04-27 01:06:26 +0200

    Implemented custom main menu style, and tried replacing Interlude songs. It does not work yet :pensive:

--- InertLofi.Content.MusicScene/Interlude1Scene.cs
+++ InertLofi.Content.MusicScene/Interlude1Scene.cs
@@ -1,6 +1,9 @@
-﻿using CalamityMod.Events;
+﻿using CalamityMod;
+using CalamityMod.Events;
 using CalamityMod.NPCs.HiveMind;
+using System;
 using Terraria;
+using Terraria.Audio;
 using Terraria.ModLoader;
 
 namespace InertLofi.Content.MusicScenes;
@@ -9,7 +12,7 @@ public class Interlude1Scene : ModSceneEffect
 {
     public override int Music => MusicLoader.GetMusicSlot((Mod)(object)InertLofiMod.Instance, "Assets/Music/Interlude1");
 
-    public override SceneEffectPriority Priority => SceneEffectPriority.BiomeHigh;
+    public override SceneEffectPriority Priority => SceneEffectPriority.BossHigh;
     public override float GetWeight(Player player)
     {
         return 1f;
@@ -22,14 +25,30 @@ public class Interlude1Scene : ModSceneEffect
         {
             return false;
         }
+        
+
+        // QUARANTINE
+        var calamity = cache as CalamityMod.CalamityMod;
+        if (calamity != null)
+        {
+            calamity.GetMusicFromMusicMod("Interlude1");
+        }
+        else
+        {
+            Console.WriteLine("cache is not a CalamityMod instance.");
+        }
+
         // Return true if the music that's currently playing is the Interlude song
-        bool isInterlude1Playing = Main.curMusic == MusicLoader.GetMusicSlot(cache, "Sounds/Music/Interlude1");
-        /*
+        bool isInterlude1Playing = Main.curMusic == calamity.GetMusicFromMusicMod("Interlude1");
         Main.NewText("Is Interlude1 playing? " + isInterlude1Playing);
-        Main.NewText("Interlude1 ID: " + MusicLoader.GetMusicSlot(cache, "Sounds/Music/Interlude1"));
+        Main.NewText("Interlude1 ID: " + calamity.GetMusicFromMusicMod("Interlude1"));
         Main.NewText("Currently playing ID: " + Main.curMusic);
-        */
-        //return isInterlude1Playing && !BossRushEvent.BossRushActive;
-        return false;
+        Main.NewText("Is the scene active? " + (isInterlude1Playing && !BossRushEvent.BossRushActive));
+        if ((isInterlude1Playing && !BossRushEvent.BossRushActive))
+        {
+            Main.musicBox2 = Music;
+            Main.curMusic = Music;
+        }
+        return isInterlude1Playing && !BossRushEvent.BossRushActive;
     }
 }
