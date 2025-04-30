5a1b81c4c14aa688b256286cef03513e0b61d15e
Aero-GamingPc <aerop@LAPTOP-0T9KKG8M>
2025-04-30 23:02:11 +0200

    Implemented Lantern Night and Planetoid

---
+++ InertLofi.Content.MusicScene/LanternNightScene.cs
@@ -0,0 +1,26 @@
+﻿using CalamityMod.Events;
+using CalamityMod.NPCs.HiveMind;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ModLoader;
+using Terraria;
+using System.Security.AccessControl;
+using Terraria.GameContent.Events;
+
+namespace InertLofi.InertLofi.Content.MusicScene
+{
+    public class LanternNightScene : ModSceneEffect
+    {
+        public override int Music => MusicLoader.GetMusicSlot((Mod)(object)InertLofiMod.Instance, "Assets/Music/LanternNight");
+
+        public override SceneEffectPriority Priority => SceneEffectPriority.Environment;
+
+        public override bool IsSceneEffectActive(Player player)
+        {
+            return LanternNight.LanternsUp && !BossRushEvent.BossRushActive;
+        }
+    }
+}
---
+++ InertLofi.Content.MusicScene/PlanetoidScene.cs
@@ -0,0 +1,40 @@
+﻿using CalamityMod.Events;
+using CalamityMod.NPCs.PlaguebringerGoliath;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace InertLofi.InertLofi.Content.MusicScene
+{
+    public class TileCounter : ModSystem
+    {
+        public static int DirtStoneMud = 0;
+
+        public override void TileCountsAvailable(ReadOnlySpan<int> tileCounts)
+        {
+            DirtStoneMud = tileCounts[TileID.Dirt] + tileCounts[TileID.Stone] + tileCounts[TileID.Mud];
+        }
+    }
+
+    public class PlanetoidScene : ModSceneEffect
+    {
+        public override int Music => MusicLoader.GetMusicSlot((Mod)(object)InertLofiMod.Instance, "Assets/Music/Planetoids");
+
+        public override SceneEffectPriority Priority => SceneEffectPriority.Environment;
+        public override float GetWeight(Player player)
+        {
+            return 1f;
+        }
+
+        public override bool IsSceneEffectActive(Player player)
+        {
+            return TileCounter.DirtStoneMud > 100 && player.ZoneSkyHeight && !BossRushEvent.BossRushActive;
+        }
+    }
+}
