585eca540460f28b6cc9edda6fc77ca8da92309f
Aero-GamingPc <aerop@LAPTOP-0T9KKG8M>
2025-05-19 15:56:24 +0200

    Cleaned up configuration and localization files

--- InertLofiConfig.cs
+++ InertLofiConfig.cs
@@ -14,113 +14,179 @@ namespace InertLofi
 
         public static InertLofiConfig Instance;
         public override ConfigScope Mode => ConfigScope.ClientSide;
-        // Please try to keep those alphabetically ordered...
+
+
         [Header("GlobalToggle")]
         [DefaultValue(true)]
         public bool ToggleAll { get; set; }
 
-        [Header("MusicToggles")]
-        
-        [DefaultValue(true)]
-        public bool ReplaceAbyss1 { get; set; }
-        [DefaultValue(true)]
-        public bool ReplaceAbyss2 { get; set; }
-        [DefaultValue(true)]
-        public bool ReplaceAbyss4 { get; set; }
+        ///
+
+        [Header("BossMusicToggles")]
+
+        ///
+
         [DefaultValue(true)]
         public bool ReplaceAquaticScourge { get; set; }
-        [DefaultValue(true)]
-        public bool ReplaceAstralSurface { get; set; }
-        [DefaultValue(true)]
-        public bool ReplaceAstralUnderground { get; set; }
+
         [DefaultValue(true)]
         public bool ReplaceAstrumAureus { get; set; }
+
         [DefaultValue(true)]
         public bool ReplaceAstrumDeus { get; set; }
-        [DefaultValue(true)]
-        public bool ReplaceBioLab { get; set; }
-        [DefaultValue(true)]
-        public bool ReplaceBrimCrag { get; set; }
+
         [DefaultValue(true)]
         public bool ReplaceBrimstoneElemental { get; set; }
+
         [DefaultValue(true)]
         public bool ReplaceBumblebirb { get; set; }
+
         [DefaultValue(true)]
         public bool ReplaceCalamitasPhase1 { get; set; }
+
         [DefaultValue(true)]
         public bool ReplaceCalamitasPhase2 { get; set; }
+
         [DefaultValue(true)]
         public bool ReplaceCalamitasPhase3 { get; set; }
+
         [DefaultValue(true)]
         public bool ReplaceCalamitasPhase4 { get; set; }
+
         [DefaultValue(true)]
         public bool ReplaceCalClone { get; set; }
+
         [DefaultValue(true)]
         public bool ReplaceCeaselessVoid { get; set; }
+
         [DefaultValue(true)]
         public bool ReplaceCrabulon { get; set; }
+
         [DefaultValue(true)]
         public bool ReplaceCryogen { get; set; }
+
         [DefaultValue(true)]
         public bool ReplaceDesertScourge { get; set; }
+
         [DefaultValue(true)]
         public bool ReplaceDoGPhase1 { get; set; }
+
         [DefaultValue(true)]
         public bool ReplaceDoGPhase2 { get; set; }
+
         [DefaultValue(true)]
-        public bool ReplaceHiveMind { get; set; }        
-        [DefaultValue(true)]
-        public bool ReplaceInterlude1 { get; set; }
-        [DefaultValue(true)]
-        public bool ReplaceInterlude2 { get; set; }
-        [DefaultValue(true)]
-        public bool ReplaceInterlude3 { get; set; }
-        [DefaultValue(true)]
-        public bool ReplaceInterlude4 { get; set; }
-        [DefaultValue(true)]
-        public bool ReplaceInterlude5 { get; set; }
-        [DefaultValue(true)]
-        public bool ReplaceLanternNight { get; set; }
+        public bool ReplaceHiveMind { get; set; }
+
         [DefaultValue(true)]
         public bool ReplaceLevi1 { get; set; }
+
         [DefaultValue(true)]
         public bool ReplaceLevi2 { get; set; }
+
         [DefaultValue(true)]
         public bool ReplaceLevi3 { get; set; }
+
         [DefaultValue(true)]
         public bool ReplaceOldDuke { get; set; }
+
         [DefaultValue(true)]
         public bool ReplacePerforators { get; set; }
+
         [DefaultValue(true)]
         public bool ReplacePlagueGoliath { get; set; }
-        [DefaultValue(true)]
-        public bool ReplacePlanetoid { get; set; }
+
         [DefaultValue(true)]
         public bool ReplacePolterGhast { get; set; }
+
         [DefaultValue(true)]
         public bool ReplaceProfanedGuardians { get; set; }
+
         [DefaultValue(true)]
         public bool ReplaceProvidence { get; set; }
+
         [DefaultValue(true)]
         public bool ReplaceRavager { get; set; }
+
         [DefaultValue(true)]
         public bool ReplaceSignus { get; set; }
+
         [DefaultValue(true)]
         public bool ReplaceSlimeGod { get; set; }
+
         [DefaultValue(true)]
         public bool ReplaceStormWeaver { get; set; }
-        [DefaultValue(true)]
-        public bool ReplaceSulphurSeaDay { get; set; }
-        [DefaultValue(true)]
-        public bool ReplaceSulphurSeaNight { get; set; }
-        [DefaultValue(true)]
-        public bool ReplaceSunkenSea { get; set; }
+
         [DefaultValue(true)]
         public bool ReplaceWyrm { get; set; }
+
         [DefaultValue(true)]
         public bool ReplaceYharon1 { get; set; }
+
         [DefaultValue(true)]
         public bool ReplaceYharon2 { get; set; }
+
+        ///
+
+        [Header("BiomeMusicToggles")]
+
+        ///
+
+        [DefaultValue(true)]
+        public bool ReplaceAbyss1 { get; set; }
+
+        [DefaultValue(true)]
+        public bool ReplaceAbyss2 { get; set; }
+
+        [DefaultValue(true)]
+        public bool ReplaceAbyss4 { get; set; }
+
+        [DefaultValue(true)]
+        public bool ReplaceAstralSurface { get; set; }
+
+        [DefaultValue(true)]
+        public bool ReplaceAstralUnderground { get; set; }
+
+        [DefaultValue(true)]
+        public bool ReplaceBioLab { get; set; }
+
+        [DefaultValue(true)]
+        public bool ReplaceBrimCrag { get; set; }
+
+        [DefaultValue(true)]
+        public bool ReplacePlanetoid { get; set; }
+
+        [DefaultValue(true)]
+        public bool ReplaceSulphurSeaDay { get; set; }
+
+        [DefaultValue(true)]
+        public bool ReplaceSulphurSeaNight { get; set; }
+
+        [DefaultValue(true)]
+        public bool ReplaceSunkenSea { get; set; }
+
+        ///
+
+        [Header("EventsMusicToggles")]
+
+        ///
+
+        [DefaultValue(true)]
+        public bool ReplaceInterlude1 { get; set; }
+
+        [DefaultValue(true)]
+        public bool ReplaceInterlude2 { get; set; }
+
+        [DefaultValue(true)]
+        public bool ReplaceInterlude3 { get; set; }
+
+        [DefaultValue(true)]
+        public bool ReplaceInterlude4 { get; set; }
+
+        [DefaultValue(true)]
+        public bool ReplaceInterlude5 { get; set; }
+
+        [DefaultValue(true)]
+        public bool ReplaceLanternNight { get; set; }
 
     }
 }
