51022658d899adeff8c360174c1a85b997699874
Aero-GamingPc <aerop@LAPTOP-0T9KKG8M>
2026-01-10 23:16:10 +0100

    Moved TileCounter class to its own file

--- InertLofi.Content.MusicScene/PlanetoidScene.cs
+++ InertLofi.Content.MusicScene/PlanetoidScene.cs
@@ -12,16 +12,6 @@ using Terraria.ModLoader;
 
 namespace InertLofi.InertLofi.Content.MusicScene
 {
-    public class TileCounter : ModSystem
-    {
-        public static int DirtStoneMud = 0;
-
-        public override void TileCountsAvailable(ReadOnlySpan<int> tileCounts)
-        {
-            DirtStoneMud = tileCounts[TileID.Dirt] + tileCounts[TileID.Stone] + tileCounts[TileID.Mud];
-        }
-    }
-
     public class PlanetoidScene : ModSceneEffect
     {
         public override int Music => MusicLoader.GetMusicSlot((Mod)(object)InertLofiMod.Instance, "Assets/Music/Planetoids");
---
+++ Utils/TileCounter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace InertLofi.Utils
+{
+    public class TileCounter : ModSystem
+    {
+        public static int DirtStoneMud = 0;
+
+        public static int MeteoriteOre = 0;
+
+        public override void TileCountsAvailable(ReadOnlySpan<int> tileCounts)
+        {
+            DirtStoneMud = tileCounts[TileID.Dirt] + tileCounts[TileID.Stone] + tileCounts[TileID.Mud];
+            MeteoriteOre = tileCounts[TileID.Meteorite];
+        }
+    }
+}
